bfddb9f52cd8d0615673d123533d17907a44b7a1
nbutmickey <1781196595@qq.com>
2018-08-05 16:45:16 +0800

    sql change

--- server/sql/sqlMap.js
+++ server/sql/sqlMap.js
@@ -4,7 +4,8 @@ let sqlMap={
     getArticleDetail:'SELECT * FROM articles where postId=?',
   },
   tags:{
-    getAllTags:'SELECT tag,relations.tagId,count(*) AS length FROM relations,tags WHERE relations.tagId=tags.tagId GROUP BY relations.tagId'
+    getAllTags:'SELECT tag,relations.tagId,count(*) AS length FROM relations,tags WHERE relations.tagId=tags.tagId GROUP BY relations.tagId\n' +
+    ',tags.tag'
   }
 };
 
