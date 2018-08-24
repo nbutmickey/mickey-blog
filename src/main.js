abd2788c49cbdcff9df0920eb018db23f1647a09
nbutmickey <1781196595@qq.com>
2018-08-24 15:45:01 +0800

    segamentfault代码高亮的样式

--- src/main.js
+++ src/main.js
@@ -9,12 +9,12 @@ import {library} from '@fortawesome/fontawesome-svg-core'
 import {faHome,faTags,faArchive,faUser,faCalendarTimes,faFolder,faEye,faCommentAlt,faCheckSquare} from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios'
 import  hljs from 'highlight.js'
-import 'highlight.js/styles/solarized-light.css'
+import 'highlight.js/styles/solarized-dark.css'
 Vue.directive('highlight',function (el) {
   let blocks = el.querySelectorAll('pre code');
   blocks.forEach((block)=>{
     hljs.highlightBlock(block)
-  })
+})
 });
 
 import NProgress from 'nprogress'
